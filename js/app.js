9866cedf9de75dc1a5730b38d804bcd0e1d30470
MMadejsza <maciej.madejsza@gmail.com>
2022-12-19 15:44:44 +0000

    Fetch done. Breeds to console

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('works');
+function fetchBreeds() {
+	return fetch('https://dog.ceo/api/breeds/list/all')
+		.then((resp) => resp.json())
+		.then((data) => {
+			return data.message;
+		});
+}
+fetchBreeds().then((messageAsBreeds) => console.log(messageAsBreeds));

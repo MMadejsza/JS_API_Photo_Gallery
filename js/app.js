57a4a0e0523bc9b899a7ab61d12c76db2d7d582f
MMadejsza <maciej.madejsza@gmail.com>
2022-12-19 16:06:36 +0000

    Transforming to class. Methods, constructor and api url

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
-function fetchBreeds() {
-	return fetch('https://dog.ceo/api/breeds/list/all')
-		.then((resp) => resp.json())
-		.then((data) => {
-			return data.message;
-		});
+class Dog {
+	constructor() {
+		this.apiUrl = 'https://dog.ceo/api';
+	}
+
+	fetchBreeds() {
+		return fetch(`${this.apiUrl}/breeds/list/all`)
+			.then((resp) => resp.json())
+			.then((data) => {
+				return data.message;
+			});
+	}
+
+	getRandomDogImage() {
+		return fetch(`${this.apiUrl}/api/breeds/image/random`)
+			.then((resp) => resp.json())
+			.then((data) => data.message);
+	}
+
+	getRandomDogImageByBreed(breed) {
+		return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+			.then((resp) => resp.json())
+			.then((data) => data.message);
+	}
 }
-fetchBreeds().then((messageAsBreeds) => console.log(messageAsBreeds));
-
-function getRandomDogImage() {
-	return fetch('https://dog.ceo/api/breeds/image/random')
-		.then((resp) => resp.json())
-		.then((data) => data.message);
-}
-
-const exampleImg = document.querySelector('img');
-
-function getRandomDogImageByBreed(breed) {
-	return fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-		.then((resp) => resp.json())
-		.then((data) => data.message);
-}
-
-getRandomDogImageByBreed('boxer').then((src) => exampleImg.setAttribute('src', src));
